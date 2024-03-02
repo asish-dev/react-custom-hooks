9624c3ed0190d4b2866973e034a373ddbb8879da
asish-dev <asish-dev@gmail.com>
2024-03-02 15:32:57 +0530

    vite react setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import styles from './Layout.module.scss';
+
+export const metadata = {
+  title: 'My Hooks - asish',
+  description: 'A set of custom hooks',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className={styles.AppLayout}>{children}</div>;
+}
