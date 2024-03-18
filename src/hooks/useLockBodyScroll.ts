c8465189cb99b5dcd7f3880bb317fbc92173c499
asish-dev <asish043@gmail.com>
2024-03-18 23:22:13 +0530

    useLockBodyScroll

---
+++ src/hooks/useLockBodyScroll.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function useLockBodyScroll() {
+  React.useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
+  }, []);
+}
