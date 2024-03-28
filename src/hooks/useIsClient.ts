3ff55e29319c443b432e3cf19931318d36ab9742
asish-dev <asish043@gmail.com>
2024-03-29 02:06:09 +0530

    useIsClient

---
+++ src/hooks/useIsClient.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function useIsClient() {
+  const [isClient, setIsClient] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+}
